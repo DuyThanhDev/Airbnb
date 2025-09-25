41a9a007aa788b2c85a8de416c662ff032a618c4
Apes <21040431.thanh@student.iuh.edu.vn>
2025-09-25 20:28:46 +0700

    Individual Listing View

--- airbnb/src/app/types/index.ts
+++ airbnb/src/app/types/index.ts
@@ -1,7 +1,14 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
+
+export type safeListings = Omit<
+    Listing,
+    "createdAt"
+> & {
+    createdAt: string;
+}
 
 export type SafeUser = Omit<
-User,
+    User,
     "createdAt" | "updatedAt" | "emailVerified"
 > & {
     createdAt: string;
